2c5162c1c0bf7191bed682b9e87192d9c2de3e9b
MaguinhoD <vitor.amorim@fatec.sp.gov.br>
2021-02-23 09:45:06 -0300

    Commit Inicial

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, Response, request
+from flask_sqlalchemy import
+import mysql.connector
+import json
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/LES'
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+
+
+
+
+# CRUDE
