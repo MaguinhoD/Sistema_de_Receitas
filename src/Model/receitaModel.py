e0654b4872b28d84206b98fb48e5046f02e4a483
MaguinhoD <vitor.amorim@fatec.sp.gov.br>
2021-02-23 10:59:16 -0300

    Adiconado modelo pronto para testes

--- src/Model/receitaModel.py
+++ src/Model/receitaModel.py
@@ -2,10 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Usuario(db.Model):
+class Receita(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    steps = db.Column(db.Integer, nullable=False)
+    ingredients = db.Column(db.String(1000), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
