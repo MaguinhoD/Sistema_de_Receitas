d2c213f3135b00162ce09d769763f6e9c8404dc1
MaguinhoD <vitor.amorim@fatec.sp.gov.br>
2021-03-16 12:03:50 -0300

    Adicionado Login Pop-up

--- src/Viewer/Header/header.js
+++ src/Viewer/Header/header.js
@@ -2,7 +2,13 @@ function logout() {
     localStorage.removeItem('user_id')
     window.location.href = '/';
 }
-
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
 
 function sendLogin() {
     let email = document.getElementById('email');
